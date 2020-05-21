2f772ad8c519102e4dbd308085098e17fd7d129b
marioandreseche@gmail.com <marioecheverrialopez@MacBook-Pro-de-Mario.local>
2020-05-21 17:31:13 -0400

    first commit

---
+++ src/app/table-list/table-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+interface Food {
+  value: string;
+  viewValue: string;
+}
+@Component({
+  selector: "app-table-list",
+  templateUrl: "./table-list.component.html",
+  styleUrls: ["./table-list.component.css"],
+})
+export class TableListComponent implements OnInit {
+  foods: Food[] = [
+    { value: "steak-0", viewValue: "Steak" },
+    { value: "pizza-1", viewValue: "Pizza" },
+    { value: "tacos-2", viewValue: "Tacos" },
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+interface Food {
+  value: string;
+  viewValue: string;
+}
+@Component({
+  selector: "app-user-profile",
+  templateUrl: "./user-profile.component.html",
+  styleUrls: ["./user-profile.component.css"],
+})
+export class UserProfileComponent implements OnInit {
+  foods: Food[] = [
+    { value: "steak-0", viewValue: "Steak" },
+    { value: "pizza-1", viewValue: "Pizza" },
+    { value: "tacos-2", viewValue: "Tacos" },
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+}
